930e7a315911750384ca2292739b7f920348c75b
Denis Valdivieso <sshshaka@gmail.com>
2021-11-01 22:27:24 -0400

    Componentes Header & Form

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import Header from "./components/Header";
+import Form from "./components/Form";
+
 function App() {
+  const [data, setData] = useState({
+    city: "",
+    country: "",
+  });
+
   return (
     <>
-      <h1>Clima React</h1>
+      <Header titulo="Clima React App" />
+      <div className="contenedor-form">
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form data={data} setData={setData} />
+            </div>
+            <div className="col m6 s12">2</div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+const Form = ({ data, setData }) => {
+  const [error, setError] = useState(false);
+  const { city, country } = data;
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (city.trim() === "" || country.trim() === "") {
+      setError(true);
+      return;
+    }
+
+    setError(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error ? <p className="error">Ambos campos son obligatorios</p> : null}
+
+      <div className="input-field col s12">
+        <input
+          type="text"
+          name="city"
+          id="city"
+          value={city}
+          onChange={handleChange}
+        />
+        <label htmlFor="city">Ciudad: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <select
+          name="country"
+          id="country"
+          value={country}
+          onChange={handleChange}
+        >
+          <option value="">-- Selecciona un Pais --</option>
+          <option value="AR">Argentina</option>
+          <option value="BO">Bolivia</option>
+          <option value="CO">Brasil</option>
+          <option value="CL">Chile</option>
+          <option value="CO">Colombia</option>
+          <option value="EC">Ecuador</option>
+          <option value="PY">Paraguay</option>
+          <option value="PE">Peru</option>
+          <option value="UY">Uruguay</option>
+          <option value="VE">Venezuela</option>
+        </select>
+        <label htmlFor="country">País: </label>
+      </div>
+
+      <div className="input-field col s12">
+        <input
+          type="submit"
+          value="Buscar Clima"
+          className="waves-effect waves-light btn-large btn-block yellow accent-4"
+        />
+      </div>
+    </form>
+  );
+};
+
+export default Form;

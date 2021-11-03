ef3d3ba607dd6c6eec73f60188b057e8bb37c017
Denis Valdivieso <sshshaka@gmail.com>
2021-11-03 09:15:59 -0400

    Manejo de Errores con el componente Error
    - bug fixed - fetchApi()

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Form from "./components/Form";
+import Error from "./components/Error";
 import Weather from "./components/Weather";
 
 function App() {
@@ -11,20 +12,38 @@ function App() {
 
   const [query, setQuery] = useState(false);
   const [result, setResult] = useState({});
+  const [error, setError] = useState(false);
 
   const { city, country } = data;
 
   useEffect(() => {
     const fetchAPI = async () => {
-      const apiKey = "b234cf14d1e1e4bfd82560a37d25401c";
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
-      const res = await fetch(url);
-      const json = await res.json();
-      setResult(json);
-      setQuery(false);
+      if (query) {
+        const apiKey = "b234cf14d1e1e4bfd82560a37d25401c";
+        const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${apiKey}`;
+        const res = await fetch(url);
+        const json = await res.json();
+        setResult(json);
+        setQuery(false);
+      }
     };
-    if (query) fetchAPI();
-  }, [city, country, query]);
+
+    if (result.cod === "404") {
+      setError(true);
+    } else {
+      setError(false);
+    }
+
+    fetchAPI();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [query]);
+
+  let component;
+  if (error) {
+    component = <Error msg="No hay resultados" />;
+  } else {
+    component = <Weather result={result} />;
+  }
 
   return (
     <>
@@ -35,9 +54,7 @@ function App() {
             <div className="col m6 s12">
               <Form data={data} setData={setData} setQuery={setQuery} />
             </div>
-            <div className="col m6 s12">
-              <Weather result={result} />
-            </div>
+            <div className="col m6 s12">{component}</div>
           </div>
         </div>
       </div>
---
+++ src/components/Error.js
@@ -0,0 +1,3 @@
+const Error = ({ msg }) => <p className="red darken-4 error">{msg}</p>;
+
+export default Error;
